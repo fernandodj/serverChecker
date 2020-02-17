ae34a97b1676f3c7c9eb348ce7c2fd31a8490ef5
fernandodj <ferjalid@gmail.com>
2020-02-17 15:44:14 -0300

    add post request

--- app/agent.py
+++ app/agent.py
@@ -1,48 +1,48 @@
 import subprocess
 import json
-import csv
 import pprint
 import platform
+import requests
 
 def main():
-        data = dict()
-        data.update(process_data = getProcessData())
         os_info = getOSData()
-        data.update(os_name = os_info['name'])
-        data.update(os_version = os_info['version'])
-        
-        getUsersData()
-        #getUsersData()
-        #getCPUData()
+        data = {
+                "process_data": getProcessData(),
+                "os_name": os_info['name'],
+                "os_version": os_info['version'],
+                "users": getUsersData(),
+                "cpu": getCPUData()
+        }
 
+        doRequest(data)
+        print(json.dumps(data)) 
+
+
+def doRequest(server_data):
+        r = requests.post(url = 'http://localhost:80/save', data = server_data)
+        response = r.text
+        print(response)
 
 def getCPUData():
-        """ cmd = "lscpu | grep -iE 'Nombre del modelo|ID de fabricante|Arquitectura'" """
-        output = subprocess.Popen(['lscpu'], stdout=subprocess.PIPE).stdout.readlines()
-        headers = [h for h in ' '.join(output[0].strip().split()).split() if h]
-        raw_data = map(lambda s: s.strip().split(None, len(headers) - 1), output[1:])
-        pprint.pprint([dict(zip(headers, r)) for r in raw_data])
-        return [dict(zip(headers, r)) for r in raw_data]
-        """ process = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
-        print(process) """
-        
-
+        cpu_type = subprocess.Popen(['uname','-p'], stdout=subprocess.PIPE).stdout.readlines()[0].strip('\n')
+        vendor = subprocess.Popen(["cat /proc/cpuinfo | grep 'vendor_id' | uniq"], stdout=subprocess.PIPE, shell=True).stdout.readlines()[0].split(':')
+        model_name = subprocess.Popen(["cat /proc/cpuinfo | grep 'model name' | uniq"], stdout=subprocess.PIPE, shell=True).stdout.readlines()[0].split(':')
+        cant_cpu = subprocess.Popen(["cat /proc/cpuinfo | grep processor | wc -l"], stdout=subprocess.PIPE, shell=True).stdout.readlines()[0].strip('\n') 
+        cpu_data = {
+                "type" : cpu_type,
+                "vendor":  vendor[1].strip('\n'),
+                "model": model_name[1].strip('\n'),
+                "quantity": cant_cpu
+        } 
+        return cpu_data
 
 def getUsersData():
         output = subprocess.Popen(['w'], stdout=subprocess.PIPE).stdout.readlines()
         headers = [h for h in ' '.join(output[1].strip().split()).split() if h]
         raw_data = map(lambda s: s.strip().split(None, len(headers) - 1), output[1:])
-        pprint.pprint([dict(zip(headers, r)) for r in raw_data])
         return [dict(zip(headers, r)) for r in raw_data]
-        """ w = subprocess.Popen(['who'], stdout=subprocess.PIPE)
-        data = w.stdout.read()
-        print(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))) """
     
     
-
-
-
-
 def getProcessData():
         output = subprocess.Popen(['ps', '-l'], stdout=subprocess.PIPE).stdout.readlines()
         headers = [h for h in ' '.join(output[0].strip().split()).split() if h]
@@ -50,13 +50,16 @@ def getProcessData():
         return [dict(zip(headers, r)) for r in raw_data]
 
 def getOSData():
-        os_info = dict()
         if(platform.system().lower() == 'linux' or platform.system().lower() == 'linux2'):
-                os_info.update(name = platform.dist()[0])
-                os_info.update(version = platform.dist()[1])
+                os_info = {
+                        "name": platform.dist()[0],
+                        "version": platform.dist()[1]
+                }
         else:
-                os_info.update(name = platform.system())
-                os_info.update(version = platform.release())
+                os_info = {
+                        "name": platform.system(),
+                        "version": platform.release()
+                }
         return os_info
 
 main()
