4fbadf100ec7b3ac13fe2bea1287606409e5a3c9
fernandodj <ferjalid@gmail.com>
2020-02-20 22:43:21 -0300

    update cpu info
    add usage, uptime to CPU info

--- app/agent.py
+++ app/agent.py
@@ -1,6 +1,5 @@
 import subprocess
 import json
-import pprint
 import platform
 import requests
 
@@ -12,9 +11,9 @@ def main():
                 "os_version": os_info['version'],
                 "users": getUsersData(),
                 "cpu": getCPUData()
-        }
+        } 
 
-        doRequest(data)
+        #doRequest(data)
         print(json.dumps(data)) 
 
 
@@ -25,20 +24,20 @@ def doRequest(server_data):
 
 def getCPUData():
         cpu_type = subprocess.Popen(['uname','-p'], stdout=subprocess.PIPE).stdout.readlines()[0].strip('\n')
-        vendor = subprocess.Popen(["cat /proc/cpuinfo | grep 'vendor_id' | uniq"], stdout=subprocess.PIPE, shell=True).stdout.readlines()[0].split(':')
         model_name = subprocess.Popen(["cat /proc/cpuinfo | grep 'model name' | uniq"], stdout=subprocess.PIPE, shell=True).stdout.readlines()[0].split(':')
-        cant_cpu = subprocess.Popen(["cat /proc/cpuinfo | grep processor | wc -l"], stdout=subprocess.PIPE, shell=True).stdout.readlines()[0].strip('\n') 
+        usage = subprocess.Popen(["top -bn1 | awk '/Cpu/ { cpu =  100 - $8 }; END { print cpu }'"], stdout=subprocess.PIPE, shell=True).stdout.readlines()[0].split(',  ')
+        uptime_active = subprocess.Popen(['uptime -p'], stdout=subprocess.PIPE, shell=True).stdout.readlines()[0].strip('up')
         cpu_data = {
                 "type" : cpu_type,
-                "vendor":  vendor[1].strip('\n'),
                 "model": model_name[1].strip('\n'),
-                "quantity": cant_cpu
+                "usage": usage[0].replace('\n', '%'),
+                "uptime": uptime_active.strip('\n')
         } 
         return cpu_data
 
 def getUsersData():
-        output = subprocess.Popen(['w'], stdout=subprocess.PIPE).stdout.readlines()
-        headers = [h for h in ' '.join(output[1].strip().split()).split() if h]
+        output = subprocess.Popen(["who | awk 'BEGIN {print \"NAME LINE DATE HOUR\"} {if(NR > 1) print $1, $2, $3, $4}'"], stdout=subprocess.PIPE, shell=True).stdout.readlines()
+        headers = [h for h in ' '.join(output[0].strip().split()).split() if h]
         raw_data = map(lambda s: s.strip().split(None, len(headers) - 1), output[1:])
         return [dict(zip(headers, r)) for r in raw_data]
     
